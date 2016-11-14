d13ddbb1d0810d8bf78edf1f3555f2f7eee32a67
Akash <akashrajguru@icloud.com>
2016-11-14 15:35:45 +0000

    initial commit

---
+++ WebContent/asset/js/custom.js
@@ -0,0 +1,12 @@
+/* Filename: custom.js */
+$(document).ready(function(){
+	
+		$(".select-btn").click(function(){
+			 window.location.href='adminlogin.jsp';
+			//alert("This from custom document...");
+			/*var value = $(this).attr("productid");
+			var url = "details.html?id="+value;
+			window.location.href= url;*/
+		});
+
+});

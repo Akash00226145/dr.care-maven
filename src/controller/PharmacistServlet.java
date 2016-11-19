c3424d781d1541eb6467fa67971612980f2adab9
Renrui Zhang <a00215458@student.ait.ie>
2016-11-19 17:52:41 +0800

    Pharmacist login function finish

--- src/controller/PharmacistServlet.java
+++ src/controller/PharmacistServlet.java
@@ -1,41 +1,62 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.AdminUser;
+import model.Pharmacist;
+import dao.AdminUserDAO;
+import dao.PharmacistDAO;
 
 /**
- * Servlet implementation class PharmacistServlet
+ * Servlet implementation class HelloControllerServlet
  */
 @WebServlet("/PharmacistServlet")
 public class PharmacistServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PharmacistServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+   private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+   /**
+    * @see HttpServlet#HttpServlet()
+    */
+   public PharmacistServlet() {
+      super();
+      // TODO Auto-generated constructor stub
+   }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+   /**
+    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+    *      response)
+    */
+   protected void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      // TOD
+   }
+
+   /**
+    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+    *      response)
+    */
+   protected void doPost(HttpServletRequest request,
+         HttpServletResponse response) throws ServletException, IOException {
+     
+	   String username = request.getParameter("username");
+       String password = request.getParameter("password");
+       Pharmacist phar =PharmacistDAO.instance.checkLogin(username, password);
+      if (phar != null) {
+    	  System.out.println("Yes password mached");
+         HttpSession session = request.getSession();
+         session.setAttribute("phar", phar);
+         request.getRequestDispatcher("success.jsp").forward(request, response);
+      } else {
+         request.getRequestDispatcher("index.jsp").forward(request, response);
+         System.out.println("not mached");
+      }
+   }
 
 }
